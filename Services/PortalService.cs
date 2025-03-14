432dd4d8b22d0e1b442ace3a53c41ec2f9abe19e
Evandro Almeida <89evandroalmeida@gmail.com>
2025-03-14 16:16:58 -0300

    Lógica de conversão de todas as abas da planilha mensal

--- Services/PortalService.cs
+++ Services/PortalService.cs
@@ -13,23 +13,114 @@ namespace TransparenciaWindows.Services
             using (var planilha = ExcelReaderFactory.CreateReader(dadosPlanilha))
             {
                 var ds = planilha.AsDataSet();
+                var dadosEncargos = ConverterDadosEncargos(ds.Tables[1]);
                 var dadosPessoais = ConverterDadosPessoais(ds.Tables[2]);
-                // The result of each spreadsheet is in result.Tables
+                var dadosFinanceiros = ConverterDadosFinanceiros(ds.Tables[3]);
+                var dadosCargos = ConverterDadosCargos(ds.Tables[4]);
+                var dadosVencimentos = ConverterDadosVencimentos(ds.Tables[5]);
+                var dadosUnidades = ConverterDadosUnidades(ds.Tables[6]);
 
                 string baseDirectory = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\Outputs");
                 string filePath = Path.Combine(baseDirectory, "portal.txt");
 
                 using (StreamWriter saida = new StreamWriter(filePath))
                 {
+                    saida.Write(dadosEncargos);
                     saida.Write(dadosPessoais);
+                    saida.Write(dadosFinanceiros);
+                    saida.Write(dadosCargos);
+                    saida.Write(dadosVencimentos);
+                    saida.Write(dadosUnidades);
                     MessageBox.Show("Arquivo gerado com sucesso!");
                 }
             }
         }
 
+        private static String ConverterDadosEncargos(DataTable aba)
+        {
+            string conteudo = "";
+            for (int i = 1; i < aba.Rows.Count; i++)
+            {
+                DataRow registro = aba.Rows[i];
+
+                string linhaFormatada = "";
+                linhaFormatada += "ES".PadRight(2); /* [A] C 02 */
+                linhaFormatada += $"{i}".PadLeft(7, '0'); /* [B] Z 07 */
+                linhaFormatada += $"{registro[2]}".Trim().PadLeft(2, '0'); /* [C] Z 02 */
+                linhaFormatada += $"{registro[3]}".Trim().PadLeft(3, '0'); /* [D] Z 03 */
+                linhaFormatada += "".PadRight(33); /* [E] C 33 */
+                linhaFormatada += $"{registro[5]}".Split(' ')[0].Trim().PadLeft(6,'0'); /* [F] Z 06 */
+                linhaFormatada += $"{registro[6]}".Trim().PadLeft(15,'0'); /* [G] Z 15 */
+                linhaFormatada += $"{registro[7]}".Split(' ')[0].Trim().PadLeft(6,'0'); /* [H] Z 06 */
+                linhaFormatada += $"{registro[8]}".Trim().PadLeft(15,'0'); /* [I] Z 15 */
+                linhaFormatada += $"{registro[9]}".Split(' ')[0].Trim().PadLeft(6,'0'); /* [J] Z 06 */
+                linhaFormatada += $"{registro[10]}".Trim().PadLeft(15,'0'); /* [K] Z 15 */
+                linhaFormatada += $"{registro[11]}".Split(' ')[0].Trim().PadLeft(6,'0'); /* [L] Z 06 */
+                linhaFormatada += $"{registro[12]}".Trim().PadLeft(15,'0'); /* [M] Z 15 */
+                linhaFormatada += $"{registro[13]}".Split(' ')[0].Trim().PadLeft(6,'0'); /* [N] Z 06 */
+                linhaFormatada += $"{registro[14]}".Trim().PadLeft(15,'0'); /* [O] Z 15 */
+                linhaFormatada += $"{registro[15]}".Split(' ')[0].Trim().PadLeft(6,'0'); /* [P] Z 06 */
+                linhaFormatada += $"{registro[16]}".Trim().PadLeft(15,'0'); /* [Q] Z 15 */
+                linhaFormatada += $"{registro[17]}".Split(' ')[0].Trim().PadLeft(6,'0'); /* [R] Z 06 */
+                linhaFormatada += $"{registro[18]}".Trim().PadLeft(15,'0'); /* [S] Z 15 */
+                linhaFormatada += $"{registro[19]}".Split(' ')[0].Trim().PadLeft(6,'0'); /* [T] Z 06 */
+                linhaFormatada += $"{registro[20]}".Trim().PadLeft(15,'0'); /* [U] Z 15 */
+                linhaFormatada += $"{registro[21]}".Split(' ')[0].Trim().PadLeft(6,'0'); /* [V] Z 06 */
+                linhaFormatada += $"{registro[22]}".Trim().PadLeft(15,'0'); /* [W] Z 15 */
+                linhaFormatada += $"{registro[23]}".Split(' ')[0].Trim().PadLeft(6,'0'); /* [X] Z 06 */
+                linhaFormatada += $"{registro[24]}".Trim().PadLeft(15,'0'); /* [Y] Z 15 */
+                linhaFormatada += $"{registro[25]}".Split(' ')[0].Trim().PadLeft(6,'0'); /* [Z] Z 06 */
+                linhaFormatada += $"{registro[26]}".Trim().PadLeft(15,'0'); /* [AA] Z 15 */
+                linhaFormatada += $"{registro[27]}".Split(' ')[0].Trim().PadLeft(6,'0'); /* [AB] Z 06 */
+                linhaFormatada += $"{registro[28]}".Trim().PadLeft(15,'0'); /* [AC] Z 15 */
+                linhaFormatada += $"{registro[29]}".Split(' ')[0].Trim().PadLeft(6,'0'); /* [AD] Z 06 */
+                linhaFormatada += $"{registro[30]}".Trim().PadLeft(15,'0'); /* [AE] Z 15 */
+                linhaFormatada += $"{registro[31]}".Split(' ')[0].Trim().PadLeft(6,'0'); /* [AF] Z 06 */
+                linhaFormatada += $"{registro[32]}".Trim().PadLeft(15,'0'); /* [AG] Z 15 */
+                linhaFormatada += $"{registro[33]}".Split(' ')[0].Trim().PadLeft(6,'0'); /* [AH] Z 06 */
+                linhaFormatada += $"{registro[34]}".Trim().PadLeft(15,'0'); /* [AI] Z 15 */
+                linhaFormatada += $"{registro[35]}".Split(' ')[0].Trim().PadLeft(6,'0'); /* [AJ] Z 06 */
+                linhaFormatada += $"{registro[36]}".Trim().PadLeft(15,'0'); /* [AK] Z 15 */
+                linhaFormatada += $"{registro[37]}".Split(' ')[0].Trim().PadLeft(6,'0'); /* [AL] Z 06 */
+                linhaFormatada += $"{registro[38]}".Trim().PadLeft(15,'0'); /* [AM] Z 15 */
+                linhaFormatada += $"{registro[39]}".Split(' ')[0].Trim().PadLeft(6,'0'); /* [AN] Z 06 */
+                linhaFormatada += $"{registro[40]}".Trim().PadLeft(15,'0'); /* [AO] Z 15 */
+                linhaFormatada += $"{registro[41]}".Split(' ')[0].Trim().PadLeft(6,'0'); /* [AP] Z 06 */
+                linhaFormatada += $"{registro[42]}".Trim().PadLeft(15,'0'); /* [AQ] Z 15 */
+                linhaFormatada += $"{registro[43]}".Split(' ')[0].Trim().PadLeft(6,'0'); /* [AR] Z 06 */
+                linhaFormatada += $"{registro[44]}".Trim().PadLeft(15,'0'); /* [AS] Z 15 */
+                linhaFormatada += $"{registro[45]}".Split(' ')[0].Trim().PadLeft(6,'0'); /* [AT] Z 06 */
+                linhaFormatada += $"{registro[46]}".Trim().PadLeft(15,'0'); /* [AU] Z 15 */
+                linhaFormatada += $"{registro[47]}".Split(' ')[0].Trim().PadLeft(6,'0'); /* [AV] Z 06 */
+                linhaFormatada += $"{registro[48]}".Trim().PadLeft(15,'0'); /* [AW] Z 15 */
+                linhaFormatada += $"{registro[49]}".Split(' ')[0].Trim().PadLeft(6,'0'); /* [AX] Z 06 */
+                linhaFormatada += $"{registro[50]}".Trim().PadLeft(15,'0'); /* [AY] Z 15 */
+                linhaFormatada += $"{registro[51]}".Split(' ')[0].Trim().PadLeft(6,'0'); /* [AZ] Z 06 */
+                linhaFormatada += $"{registro[52]}".Trim().PadLeft(15,'0'); /* [BA] Z 15 */
+                linhaFormatada += $"{registro[53]}".Split(' ')[0].Trim().PadLeft(6,'0'); /* [BB] Z 06 */
+                linhaFormatada += $"{registro[54]}".Trim().PadLeft(15,'0'); /* [BC] Z 15 */
+                linhaFormatada += $"{registro[55]}".Split(' ')[0].Trim().PadLeft(6,'0'); /* [BD] Z 06 */
+                linhaFormatada += $"{registro[56]}".Trim().PadLeft(15,'0'); /* [BE] Z 15 */
+                linhaFormatada += $"{registro[57]}".Split(' ')[0].Trim().PadLeft(6,'0'); /* [BF] Z 06 */
+                linhaFormatada += $"{registro[58]}".Trim().PadLeft(15,'0'); /* [BG] Z 15 */
+                linhaFormatada += $"{registro[59]}".Split(' ')[0].Trim().PadLeft(6,'0'); /* [BH] Z 06 */
+                linhaFormatada += $"{registro[60]}".Trim().PadLeft(15,'0'); /* [BI] Z 15 */
+                linhaFormatada += $"{registro[61]}".Split(' ')[0].Trim().PadLeft(6,'0'); /* [BJ] Z 06 */
+                linhaFormatada += $"{registro[62]}".Trim().PadLeft(15,'0'); /* [BK] Z 15 */
+                linhaFormatada += $"{registro[63]}".Split(' ')[0].Trim().PadLeft(6,'0'); /* [BL] Z 06 */
+                linhaFormatada += $"{registro[64]}".Trim().PadLeft(15,'0'); /* [BM] Z 15 */
+                linhaFormatada += "".PadRight(72); /* [BN] C 73 */
+
+                conteudo += $"{linhaFormatada}\n";
+            }
+         
+            return conteudo;
+
+        }
+
         private static String ConverterDadosPessoais(DataTable aba)
         {
-            String conteudo = "";
+            string conteudo = "";
             for (int i = 1; i < aba.Rows.Count; i++)
             {
                 DataRow registro = aba.Rows[i];
@@ -180,9 +271,150 @@ namespace TransparenciaWindows.Services
   
                 conteudo += $"{linhaFormatada}\n";
             }
-            
          
-            return conteudo;         
+            return conteudo;
         }
+
+        private static String ConverterDadosFinanceiros(DataTable aba)
+        {
+            string conteudo = "";
+            for (int i = 1; i < aba.Rows.Count; i++)
+            {
+                DataRow registro = aba.Rows[i];
+
+                string linhaFormatada = "";
+                linhaFormatada += "F2".PadRight(2); /* [A] C 02 */
+                linhaFormatada += $"{i}".PadLeft(7,'0'); /* [B] Z 07 */
+                linhaFormatada += $"{registro[2]}".Trim().PadLeft(2,'0'); /* [C] Z 02 */
+                linhaFormatada += $"{registro[3]}".Trim().PadLeft(3,'0'); /* [D] Z 03 */
+                linhaFormatada += $"{registro[4]}".Trim().PadLeft(3,'0'); /* [E] Z 03 */
+                linhaFormatada += $"{registro[5]}".Trim().PadLeft(7,'0'); /* [F] Z 07 */
+                linhaFormatada += "".PadRight(5); /* [G] C 05 */
+                linhaFormatada += $"{registro[7]}".Split(' ')[0].Trim().PadLeft(2,'0'); /* [H] Z 02 */
+                linhaFormatada += $"{registro[8]}".Split(' ')[0].Trim().PadLeft(4,'0'); /* [I] Z 04 */
+                linhaFormatada += $"{registro[9]}".Trim().PadLeft(10,'0'); /* [J] Z 10 */
+                linhaFormatada += $"{registro[10]}".Trim().PadLeft(2,'0'); /* [K] Z 02 */
+
+                // a estrutura a seguir se repete por 30 entradas
+                var index = 11;
+                for (int j = 0; j < 30; j++)
+                {
+                    linhaFormatada += $"{registro[index]}".Trim().PadLeft(6,'0'); /* [L] Z 06 */
+                    linhaFormatada += $"{registro[index + 1]}".Trim().PadLeft(13,'0'); /* [M] Z 13 */
+                    linhaFormatada += $"{registro[index + 2]}".Split(' ')[0].Trim().PadRight(1); /* [N] C 01 */
+                    linhaFormatada += $"{registro[index + 3]}".Split(' ')[0].Trim().PadRight(1); /* [O] C 01 */
+                    linhaFormatada += $"{registro[index + 4]}".Split(' ')[0].Trim().PadRight(1); /* [P] C 01 */
+                    index += 8;
+                }
+
+                linhaFormatada += "".PadRight(18); /* [JA] C 19 */
+                conteudo += $"{linhaFormatada}\n";
+            }
+
+            return conteudo;
+        }
+
+        private static String ConverterDadosCargos(DataTable aba)
+        {
+            string conteudo = "";
+            for (int i = 1; i < aba.Rows.Count; i++)
+            {
+                DataRow registro = aba.Rows[i];
+
+                string linhaFormatada = "";
+                linhaFormatada += "TC".PadRight(2); /* [A] C 02 */
+                linhaFormatada += $"{i}".PadLeft(7,'0'); /* [B] Z 07 */
+                linhaFormatada += $"{registro[2]}".Trim().PadLeft(2, '0'); /* [C] Z 02 */
+                linhaFormatada += $"{registro[3]}".Trim().PadLeft(3, '0'); /* [D] Z 03 */
+                linhaFormatada += "".PadRight(33); /* [E] C 33 */
+                linhaFormatada += $"{registro[5]}".Trim().PadLeft(7,'0'); /* [F] Z 07 */
+                linhaFormatada += $"{registro[6]}".Trim().PadRight(30); /* [G] C 30 */
+                linhaFormatada += "".PadRight(665); /* [H] C 666 */
+
+                conteudo += $"{linhaFormatada}\n";
+            }
+
+            return conteudo;
+        }
+
+        private static String ConverterDadosVencimentos(DataTable aba)
+        {
+            string conteudo = "";
+            for (int i = 1; i < aba.Rows.Count; i++)
+            {
+                DataRow registro = aba.Rows[i];
+
+                string linhaFormatada = "";
+                linhaFormatada += "TV".PadRight(2); /* [A] C 02 */
+                linhaFormatada += $"{i}".PadLeft(7,'0'); /* [B] Z 07 */
+                linhaFormatada += $"{registro[2]}".Trim().PadLeft(2,'0'); /* [C] Z 02 */
+                linhaFormatada += $"{registro[3]}".Trim().PadLeft(3,'0'); /* [D] Z 03 */
+                linhaFormatada += "".PadRight(33); /* [E] C 33 */
+                linhaFormatada += $"{registro[5]}".Trim().PadLeft(6,'0'); /* [F] Z 06 */
+                linhaFormatada += $"{registro[6]}".Trim().PadRight(30); /* [G] C 30 */
+                linhaFormatada += $"{registro[7]}".Split(' ')[0].PadRight(1); /* [H] C 01 */
+                linhaFormatada += $"{registro[8]}".Split(' ')[0].PadRight(1); /* [I] C 01 */
+                linhaFormatada += $"{registro[9]}".Split(' ')[0].PadLeft(2,'0'); /* [J] Z 02 */
+                linhaFormatada += "".PadRight(662); /* [K] C 663 */
+
+                conteudo += $"{linhaFormatada}\n";
+            }
+
+            return conteudo;
+        }
+
+        private static String ConverterDadosFaixas(DataTable aba)
+        {
+            string conteudo = "";
+            for (int i = 1; i < aba.Rows.Count; i++)
+            {
+                DataRow registro = aba.Rows[i];
+
+                string linhaFormatada = "";
+                linhaFormatada += "TF".PadRight(2); /* [A] C 02 */
+                linhaFormatada += $"{i}".PadLeft(7,'0'); /* [B] Z 07 */
+                linhaFormatada += $"{registro[2]}".Trim().PadLeft(2, '0'); /* [C] Z 02 */
+                linhaFormatada += $"{registro[3]}".Trim().PadLeft(3, '0'); /* [D] Z 03 */
+                linhaFormatada += "".PadRight(33); /* [E] C 33 */
+                linhaFormatada += $"{registro[5]}".Trim().PadRight(10); /* [F] C 10 */
+                linhaFormatada += $"{registro[6]}".Trim().PadLeft(13, '0'); /*[G] Z 13 */
+                linhaFormatada += $"{registro[7]}".Trim().PadLeft(5, '0'); /* [H] Z 05 */
+                linhaFormatada += $"{registro[8]}".Trim().PadLeft(13, '0'); /* [I] Z 13 */
+                linhaFormatada += $"{registro[9]}".Trim().PadLeft(5, '0'); /* [J] Z 05 */
+                linhaFormatada += $"{registro[10]}".Trim().PadLeft(13, '0'); /* [K] Z 13 */
+                linhaFormatada += $"{registro[11]}".Trim().PadLeft(5, '0'); /* [L] Z 05 */
+                linhaFormatada += $"{registro[12]}".Trim().PadLeft(13, '0'); /* [M] Z 13 */
+                linhaFormatada += $"{registro[13]}".Trim().PadLeft(5, '0'); /* [N] Z 05 */
+                linhaFormatada += "".PadRight(620); /* [O] C 621 */
+                conteudo += $"{linhaFormatada}\n";
+            }
+
+            return conteudo;
+        }
+
+        private static String ConverterDadosUnidades(DataTable aba)
+        {
+            string conteudo = "";
+            for (int i = 1; i < aba.Rows.Count; i++)
+            {
+                DataRow registro = aba.Rows[i];
+
+                string linhaFormatada = "";
+                linhaFormatada += "TU".PadRight(2); /* [A] C 02 */
+                linhaFormatada += $"{i}".PadLeft(7,'0'); /* [B] Z 07 */
+                linhaFormatada += $"{registro[2]}".Trim().PadLeft(2,'0'); /* [C] Z 02 */
+                linhaFormatada += $"{registro[3]}".Trim().PadLeft(3,'0'); /* [D] Z 03 */
+                linhaFormatada += "".PadRight(33); /* [E] C 33 */
+                linhaFormatada += $"{registro[5]}".Trim().PadLeft(3,'0'); /* [F] Z 03 */
+                linhaFormatada += $"{registro[6]}".Trim().PadLeft(7,'0'); /* [G] Z 07 */
+                linhaFormatada += $"{registro[7]}".Trim().PadLeft(70,'0'); /* [H] C 70 */
+                linhaFormatada += $"{registro[8]}".Trim().PadLeft(15,'0'); /* [I] Z 15 */
+                linhaFormatada += $"{registro[9]}".Trim().PadLeft(70,'0'); /* [J] C 70 */
+                linhaFormatada += "".PadRight(537); /* [K] C 538 */
+                conteudo += $"{linhaFormatada}\n";
+            }
+
+            return conteudo;
+            }
     }
 }
