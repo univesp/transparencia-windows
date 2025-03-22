6a6d6071e29efef36cc2f99a9e9bb9bde6031c2c
Evandro Almeida <89evandroalmeida@gmail.com>
2025-03-22 15:51:18 -0300

    Ajustes para o primeiro deploy

--- Forms/Form1.Designer.cs
+++ Forms/Form1.Designer.cs
@@ -56,7 +56,7 @@ namespace TransparenciaWindows
             // 
             this.btnCarregarPlanilhaMensal.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnCarregarPlanilhaMensal.Location = new System.Drawing.Point(21, 321);
-            this.btnCarregarPlanilhaMensal.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnCarregarPlanilhaMensal.Margin = new System.Windows.Forms.Padding(2);
             this.btnCarregarPlanilhaMensal.Name = "btnCarregarPlanilhaMensal";
             this.btnCarregarPlanilhaMensal.Size = new System.Drawing.Size(149, 28);
             this.btnCarregarPlanilhaMensal.TabIndex = 0;
@@ -69,7 +69,7 @@ namespace TransparenciaWindows
             this.btnConverter.BackColor = System.Drawing.SystemColors.ControlLightLight;
             this.btnConverter.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnConverter.Location = new System.Drawing.Point(22, 610);
-            this.btnConverter.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnConverter.Margin = new System.Windows.Forms.Padding(2);
             this.btnConverter.Name = "btnConverter";
             this.btnConverter.Size = new System.Drawing.Size(489, 35);
             this.btnConverter.TabIndex = 1;
@@ -89,16 +89,16 @@ namespace TransparenciaWindows
             "XML de Pagamento da Folha Ordinária (AUDESP)",
             "XML de Resumo Mensal da Folha Ordinária (AUDESP)"});
             this.cbxTipoArquivo.Location = new System.Drawing.Point(21, 556);
-            this.cbxTipoArquivo.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.cbxTipoArquivo.Margin = new System.Windows.Forms.Padding(2);
             this.cbxTipoArquivo.Name = "cbxTipoArquivo";
-            this.cbxTipoArquivo.Size = new System.Drawing.Size(489, 27);
+            this.cbxTipoArquivo.Size = new System.Drawing.Size(489, 31);
             this.cbxTipoArquivo.TabIndex = 2;
             // 
             // webCabecalho
             // 
             this.webCabecalho.AllowNavigation = false;
             this.webCabecalho.Location = new System.Drawing.Point(21, 10);
-            this.webCabecalho.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.webCabecalho.Margin = new System.Windows.Forms.Padding(2);
             this.webCabecalho.MinimumSize = new System.Drawing.Size(15, 16);
             this.webCabecalho.Name = "webCabecalho";
             this.webCabecalho.ScrollBarsEnabled = false;
@@ -112,7 +112,7 @@ namespace TransparenciaWindows
             this.label1.Location = new System.Drawing.Point(19, 288);
             this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(212, 19);
+            this.label1.Size = new System.Drawing.Size(253, 23);
             this.label1.TabIndex = 5;
             this.label1.Text = "1) Carregue a planilha mensal:";
             // 
@@ -123,7 +123,7 @@ namespace TransparenciaWindows
             this.lblCaminhoPlanilhaMensal.Location = new System.Drawing.Point(175, 326);
             this.lblCaminhoPlanilhaMensal.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblCaminhoPlanilhaMensal.Name = "lblCaminhoPlanilhaMensal";
-            this.lblCaminhoPlanilhaMensal.Size = new System.Drawing.Size(177, 19);
+            this.lblCaminhoPlanilhaMensal.Size = new System.Drawing.Size(221, 23);
             this.lblCaminhoPlanilhaMensal.TabIndex = 6;
             this.lblCaminhoPlanilhaMensal.Text = "Nenhum arquivo carregado";
             // 
@@ -134,7 +134,7 @@ namespace TransparenciaWindows
             this.label2.Location = new System.Drawing.Point(18, 525);
             this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(300, 19);
+            this.label2.Size = new System.Drawing.Size(354, 23);
             this.label2.TabIndex = 7;
             this.label2.Text = "3) Selecione o tipo de arquivo a ser gerado";
             // 
@@ -145,7 +145,7 @@ namespace TransparenciaWindows
             this.label3.Location = new System.Drawing.Point(19, 377);
             this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(298, 19);
+            this.label3.Size = new System.Drawing.Size(357, 23);
             this.label3.TabIndex = 8;
             this.label3.Text = "2) Carregue a planilha da contabilidade (*):";
             // 
@@ -156,7 +156,7 @@ namespace TransparenciaWindows
             this.label4.Location = new System.Drawing.Point(19, 453);
             this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(501, 39);
+            this.label4.Size = new System.Drawing.Size(593, 57);
             this.label4.TabIndex = 9;
             this.label4.Text = resources.GetString("label4.Text");
             this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -165,7 +165,7 @@ namespace TransparenciaWindows
             // 
             this.btnCarregarPlanilhaContabil.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnCarregarPlanilhaContabil.Location = new System.Drawing.Point(22, 410);
-            this.btnCarregarPlanilhaContabil.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnCarregarPlanilhaContabil.Margin = new System.Windows.Forms.Padding(2);
             this.btnCarregarPlanilhaContabil.Name = "btnCarregarPlanilhaContabil";
             this.btnCarregarPlanilhaContabil.Size = new System.Drawing.Size(149, 28);
             this.btnCarregarPlanilhaContabil.TabIndex = 10;
@@ -180,7 +180,7 @@ namespace TransparenciaWindows
             this.lblCaminhoPlanilhaContabil.Location = new System.Drawing.Point(175, 415);
             this.lblCaminhoPlanilhaContabil.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblCaminhoPlanilhaContabil.Name = "lblCaminhoPlanilhaContabil";
-            this.lblCaminhoPlanilhaContabil.Size = new System.Drawing.Size(177, 19);
+            this.lblCaminhoPlanilhaContabil.Size = new System.Drawing.Size(221, 23);
             this.lblCaminhoPlanilhaContabil.TabIndex = 11;
             this.lblCaminhoPlanilhaContabil.Text = "Nenhum arquivo carregado";
             // 
@@ -191,7 +191,7 @@ namespace TransparenciaWindows
             this.lnkArquivoGerado.Location = new System.Drawing.Point(26, 41);
             this.lnkArquivoGerado.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lnkArquivoGerado.Name = "lnkArquivoGerado";
-            this.lnkArquivoGerado.Size = new System.Drawing.Size(104, 19);
+            this.lnkArquivoGerado.Size = new System.Drawing.Size(128, 23);
             this.lnkArquivoGerado.TabIndex = 13;
             this.lnkArquivoGerado.TabStop = true;
             this.lnkArquivoGerado.Text = "Arquivo gerado";
@@ -209,7 +209,7 @@ namespace TransparenciaWindows
             this.pnlArquivosDownload.Controls.Add(this.lnkArquivoGerado);
             this.pnlArquivosDownload.Font = new System.Drawing.Font("Segoe UI", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.pnlArquivosDownload.Location = new System.Drawing.Point(22, 704);
-            this.pnlArquivosDownload.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pnlArquivosDownload.Margin = new System.Windows.Forms.Padding(2);
             this.pnlArquivosDownload.Name = "pnlArquivosDownload";
             this.pnlArquivosDownload.Size = new System.Drawing.Size(489, 163);
             this.pnlArquivosDownload.TabIndex = 14;
@@ -221,7 +221,7 @@ namespace TransparenciaWindows
             this.label8.Location = new System.Drawing.Point(27, 105);
             this.label8.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(434, 39);
+            this.label8.Size = new System.Drawing.Size(516, 57);
             this.label8.TabIndex = 15;
             this.label8.Text = resources.GetString("label8.Text");
             this.label8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -233,7 +233,7 @@ namespace TransparenciaWindows
             this.label7.Location = new System.Drawing.Point(9, 67);
             this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(21, 17);
+            this.label7.Size = new System.Drawing.Size(24, 20);
             this.label7.TabIndex = 17;
             this.label7.Text = "🡻";
             // 
@@ -244,7 +244,7 @@ namespace TransparenciaWindows
             this.label6.Location = new System.Drawing.Point(9, 41);
             this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(21, 17);
+            this.label6.Size = new System.Drawing.Size(24, 20);
             this.label6.TabIndex = 16;
             this.label6.Text = "🡻";
             // 
@@ -255,7 +255,7 @@ namespace TransparenciaWindows
             this.label5.Location = new System.Drawing.Point(9, 10);
             this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(185, 19);
+            this.label5.Size = new System.Drawing.Size(228, 23);
             this.label5.TabIndex = 15;
             this.label5.Text = "Arquivos para download (**):";
             // 
@@ -266,7 +266,7 @@ namespace TransparenciaWindows
             this.lnkPlanilhaMesclada.Location = new System.Drawing.Point(26, 67);
             this.lnkPlanilhaMesclada.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lnkPlanilhaMesclada.Name = "lnkPlanilhaMesclada";
-            this.lnkPlanilhaMesclada.Size = new System.Drawing.Size(116, 19);
+            this.lnkPlanilhaMesclada.Size = new System.Drawing.Size(146, 23);
             this.lnkPlanilhaMesclada.TabIndex = 14;
             this.lnkPlanilhaMesclada.TabStop = true;
             this.lnkPlanilhaMesclada.Text = "Planilha mesclada";
@@ -277,7 +277,7 @@ namespace TransparenciaWindows
             this.lnkReiniciarAplicacao.AutoSize = true;
             this.lnkReiniciarAplicacao.Location = new System.Drawing.Point(220, 665);
             this.lnkReiniciarAplicacao.Name = "lnkReiniciarAplicacao";
-            this.lnkReiniciarAplicacao.Size = new System.Drawing.Size(102, 13);
+            this.lnkReiniciarAplicacao.Size = new System.Drawing.Size(118, 19);
             this.lnkReiniciarAplicacao.TabIndex = 15;
             this.lnkReiniciarAplicacao.TabStop = true;
             this.lnkReiniciarAplicacao.Text = "Reiniciar aplicação";
@@ -285,11 +285,11 @@ namespace TransparenciaWindows
             // 
             // frmAplicacao
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 19F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoScroll = true;
             this.BackColor = System.Drawing.SystemColors.ControlLightLight;
-            this.ClientSize = new System.Drawing.Size(536, 862);
+            this.ClientSize = new System.Drawing.Size(557, 883);
             this.Controls.Add(this.lnkReiniciarAplicacao);
             this.Controls.Add(this.pnlArquivosDownload);
             this.Controls.Add(this.lblCaminhoPlanilhaContabil);
@@ -304,9 +304,9 @@ namespace TransparenciaWindows
             this.Controls.Add(this.btnConverter);
             this.Controls.Add(this.btnCarregarPlanilhaMensal);
             this.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "frmAplicacao";
-            this.Text = "Transparência UNIVESP - v1.0";
+            this.Text = "Transparência UNIVESP - v1.0.0";
             this.pnlArquivosDownload.ResumeLayout(false);
             this.pnlArquivosDownload.PerformLayout();
             this.ResumeLayout(false);
